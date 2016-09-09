652737ba516a0e82c4bd30239c63d3d4a2436300
Putri Aditya Pratama <putriaditya166@gmail.com>
2016-09-08 23:33:50 -0700

    mengganti tanggal lahir menjadi tahun lahir dan menampilkan usia

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl2004/pendaftaransiswasmktelkom/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl2004/pendaftaransiswasmktelkom/MainActivity.java
@@ -41,39 +41,104 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void doClick() {
-        StringBuilder builder = new StringBuilder();
+        if (isValid()) {
+            StringBuilder builder = new StringBuilder();
+            String nama = etn.getText().toString();
+            String tempat = ettl.getText().toString();
+            int tahun = Integer.parseInt(ettg.getText().toString());
+            int usia = 2016 - tahun;
+            String sekolah = ets.getText().toString();
+            String program = "Program yang diplih \t\n";
+            int startlen = program.length();
+            if (cbr.isChecked()) program += "\t  " + cbr.getText() + "\n";
+            if (cbb.isChecked()) program += "\t  " + cbb.getText() + "\n";
+            if (cbpsb.isChecked()) program += "\t  " + cbpsb.getText() + "\n";
+            if (program.length() == startlen) program = "Tidak ada program yang dipilih";
+            String hasil = null;
+            if (rgjk.getCheckedRadioButtonId() != -1) {
+                RadioButton rb = (RadioButton) findViewById(rgjk.getCheckedRadioButtonId());
+                hasil = rb.getText().toString();
+            }
+            builder.append("\nPendaftarn Siswa Baru SMK TELKOM\n");
+            builder.append(program);
+            builder.append("Nama : " + nama + "\n");
+            if (hasil == null) {
+                builder.append("Anda belum memilih Jenis Kelamin\n");
+            } else {
+                builder.append("Jenis Kelamin : " + hasil + "\n");
+            }
+            builder.append("Tempat Lahir : " + tempat + "\n");
+            builder.append("Tanggal Lahir : " + tahun + "\n");
+            builder.append("Umur : " + usia + "\n");
+            builder.append("Agama : ");
+            builder.append(spagama.getSelectedItem().toString() + "\n");
+            builder.append("Asal Sekolah : " + sekolah + "\n");
+
+
+            tvH.setText(builder);
+        }
+    }
+
+    private boolean isValid() {
+        boolean valid = true;
+
         String nama = etn.getText().toString();
         String tempat = ettl.getText().toString();
-        String tanggal = ettg.getText().toString();
+        String tahun = ettg.getText().toString();
         String sekolah = ets.getText().toString();
-        String program = "Program yang diplih \t\n";
-        int startlen = program.length();
-        if (cbr.isChecked()) program += "\t  " +cbr.getText() + "\n";
-        if (cbb.isChecked()) program += "\t  " +cbb.getText() + "\n";
-        if (cbpsb.isChecked()) program += "\t  " +cbpsb.getText() + "\n";
-        if (program.length() == startlen) program = "Tidak ada program yang dipilih";
-        String hasil = null;
-        if (rgjk.getCheckedRadioButtonId() != -1) {
-            RadioButton rb = (RadioButton) findViewById(rgjk.getCheckedRadioButtonId());
-            hasil = rb.getText().toString();
-        }
-        builder.append("\nPendaftarn Siswa Baru SMK TELKOM\n");
-        builder.append(program);
-        builder.append("Nama : " + nama + "\n");
-        if (hasil==null)
+
+        if(nama.isEmpty())
         {
-            builder.append("Anda belum memilih Jenis Kelamin\n");
+            etn.setError("Nama Belum diisi");
+            valid = false;
         }
-        else{
-            builder.append("Jenis Kelamin : " + hasil + "\n");
+        else if(nama.length()<3)
+        {
+            etn.setError("Nama minimal 3 karakter");
+            valid = false;
         }
-        builder.append("Tempat Lahir : " + tempat + "\n");
-        builder.append("Tanggal Lahir : " + tanggal + "\n");
-        builder.append("Agama : ");
-        builder.append(spagama.getSelectedItem().toString() + "\n");
-        builder.append("Asal Sekolah : " + sekolah + "\n");
+        else
+        {
+            etn.setError(null);
+        }
+        if(tempat.isEmpty())
+        {
+            ettl.setError("Tempat Lahir Belum diisi");
+            valid = false;
+        }
+        else
+        {
+            ettl.setError(null);
+        }
+        if(tahun.isEmpty())
+        {
+            ettg.setError("Tahun belum diisi");
+            valid = false;
+        }
+        else if (tahun.length()!=4)
+        {
+            ettg.setError("format tahun bukan yyyy");
+            valid = false;
+        }
+        else
+        {
+            ettg.setError(null);
+        }
+        if(sekolah.isEmpty())
+        {
+            ets.setError("Sekolah Belum diisi");
+            valid = false;
+        }
+        else if(sekolah.length()<5)
+        {
+            ets.setError("Nama Sekolah minimal 5 karakter");
+            valid = false;
+        }
+        else
+        {
+            ets.setError(null);
+        }
+        return valid;
 
-
-        tvH.setText(builder);
     }
 }
